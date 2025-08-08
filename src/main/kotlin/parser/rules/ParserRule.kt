99353a4c08e5e71985523a225846396c70ed02ed
vmartinbotter <vmartinbotter@mail.austral.edu.ar>
2025-08-08 18:42:42 -0300

    idea base para las rules

--- src/main/kotlin/parser/rules/ParserRule.kt
+++ src/main/kotlin/parser/rules/ParserRule.kt
@@ -3,7 +3,4 @@ package parser.rules
 import main.kotlin.lexer.Token
 import parser.matchers.Matcher
 
-interface ParserRule {
-    val matcher: Matcher<List<Token>>
-
-}
+interface ParserRule
--- src/main/kotlin/parser/rules/VariableDeclarationRule.kt
+++ src/main/kotlin/parser/rules/VariableDeclarationRule.kt
@@ -1,17 +1,22 @@
 package parser.rules
 
+import lexer.AssignmentType
+import lexer.ModifierType
 import main.kotlin.lexer.IdentifierType
 import main.kotlin.lexer.LiteralString
+import main.kotlin.lexer.PunctuationType
 import parser.matchers.SequenceMatcher
 import parser.matchers.TokenMatcher
 
 class VariableDeclarationRule : ParserRule {
-    override val matcher = SequenceMatcher(
+    val matcher = SequenceMatcher(
         listOf(
             TokenMatcher(ModifierType),
             TokenMatcher(IdentifierType),
             TokenMatcher(AssignmentType),
-            TokenMatcher(LiteralString)
+            TokenMatcher(LiteralString),
+            TokenMatcher(PunctuationType)
         )
     )
 }
+
