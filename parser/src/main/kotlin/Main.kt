4588f34c2918ce857387dd52d19c6cc3013c37bd
azulg <agarber@mail.austral.edu.ar>
2025-08-17 17:16:31 -0300

    Main

--- parser/src/main/kotlin/Main.kt
+++ parser/src/main/kotlin/Main.kt
@@ -1,27 +1,23 @@
 import lexer.TokenRule
-import main.kotlin.lexer.*            // Token, TokenProvider, DefaultLexer, ConfiguredTokens, etc.
-import rules.RuleMatcher              // tu RuleMatcher que devuelve MatchedRule
-import parser.rules.*                 // tus ParserRule: PrintlnRule, VariableDeclarationRule, ExpressionRule
+import main.kotlin.lexer.*
+import rules.RuleMatcher
+import parser.rules.*
 import rules.PrintlnRule
 
 fun main() {
 
-    // 1) Configuración del LEXER (tus reglas + ignorados)
     val baseProvider = ConfiguredTokens.providerV1()
 
     val ignored = listOf(
-        TokenRule(Regex("\\G[ \\t]+"), PunctuationType, ignore = true),      // espacios y tabs
-        TokenRule(Regex("\\G(?:\\r?\\n)+"), PunctuationType, ignore = true), // saltos de línea
-        TokenRule(Regex("\\G//.*(?:\\r?\\n|$)"), PunctuationType, ignore = true) // comentarios //
+        TokenRule(Regex("\\G[ \\t]+"), PunctuationType, ignore = true),
+        TokenRule(Regex("\\G(?:\\r?\\n)+"), PunctuationType, ignore = true),
+        TokenRule(Regex("\\G//.*(?:\\r?\\n|$)"), PunctuationType, ignore = true)
     )
 
     val tokenProvider = TokenProvider(ignored + baseProvider.rules())
     val lexer = DefaultLexer(tokenProvider)
 
-    // ⚠️ IMPORTANTE: asegurate de tener "(" y ")" en ConfiguredTokens como PunctuationType:
-    // "\\(" to PunctuationType,  "\\)" to PunctuationType
 
-    // 2) Código de prueba (elegimos casos que tus reglas ya soportan)
     val code = """
         let nombre = "Achu";
         println("hola");
@@ -36,7 +32,6 @@ fun main() {
         println("${t.type} -> '${t.value}' (linea ${t.line}, col ${t.column})")
     }
 
-    // 4) PARSER: armamos las reglas y el RuleMatcher
     val ruleMatcher = RuleMatcher(
         listOf(
             PrintlnRule(),
@@ -47,7 +42,6 @@ fun main() {
 
     val parser = DefaultParser(ruleMatcher)
 
-    // 5) PARSE: construimos el AST
     val ast = parser.parse(tokens)
 
     println("\n=== AST ===")
